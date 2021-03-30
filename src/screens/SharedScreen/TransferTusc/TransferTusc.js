ad3ae8b895ccc52adff0f98d7b982d21dc6de001
Full-Stack Developer <ai.onova1991@gmail.com>
2021-03-30 15:08:30 -0300

    fix websocket connection issue

--- src/screens/SharedScreen/TransferTusc/TransferTusc.js
+++ src/screens/SharedScreen/TransferTusc/TransferTusc.js
@@ -67,79 +67,79 @@ class TransferTusc extends Component {
                 await this.setState({ isError: false })
             }*/
             console.log("hello")
-            Apis.instance("wss://tuscapi.gambitweb.com/", true)
-            .init_promise.then((res) => {
-                console.log("connected to:", res, "network");
+            // Apis.instance("wss://tuscapi.gambitweb.com/", true)
+            // .init_promise.then((res) => {
+            //     console.log("connected to:", res, "network");
             
-                //ChainStore.init().then(() => {
+            //     //ChainStore.init().then(() => {
             
-                    let fromAccount = "marcocastiglionem";
-                    let memoSender = fromAccount;
-                    let memo = "Testing transfer from node.js";
+            //         let fromAccount = "marcocastiglionem";
+            //         let memoSender = fromAccount;
+            //         let memo = "Testing transfer from node.js";
             
-                    let toAccount = "marcocastiglione5";
+            //         let toAccount = "marcocastiglione5";
             
-                    let sendAmount = {
-                        amount: 10000,
-                        asset: "TUSC"
-                    }
+            //         let sendAmount = {
+            //             amount: 10000,
+            //             asset: "TUSC"
+            //         }
             
-                    Promise.all([
-                            FetchChain("getAccount", fromAccount),
-                            FetchChain("getAccount", toAccount),
-                            FetchChain("getAccount", memoSender),
-                            FetchChain("getAsset", sendAmount.asset),
-                            FetchChain("getAsset", sendAmount.asset)
-                        ]).then((res)=> {
-                            // console.log("got data:", res);
-                            let [fromAccount, toAccount, memoSender, sendAsset, feeAsset] = res;
+            //         Promise.all([
+            //                 FetchChain("getAccount", fromAccount),
+            //                 FetchChain("getAccount", toAccount),
+            //                 FetchChain("getAccount", memoSender),
+            //                 FetchChain("getAsset", sendAmount.asset),
+            //                 FetchChain("getAsset", sendAmount.asset)
+            //             ]).then((res)=> {
+            //                 // console.log("got data:", res);
+            //                 let [fromAccount, toAccount, memoSender, sendAsset, feeAsset] = res;
             
-                            // Memos are optional, but if you have one you need to encrypt it here
-                            //let memoFromKey = memoSender.getIn(["options","memo_key"]);
-                            //console.log("memo pub key:", memoFromKey);
-                            //let memoToKey = toAccount.getIn(["options","memo_key"]);
-                            let nonce = TransactionHelper.unique_nonce_uint64();
+            //                 // Memos are optional, but if you have one you need to encrypt it here
+            //                 //let memoFromKey = memoSender.getIn(["options","memo_key"]);
+            //                 //console.log("memo pub key:", memoFromKey);
+            //                 //let memoToKey = toAccount.getIn(["options","memo_key"]);
+            //                 let nonce = TransactionHelper.unique_nonce_uint64();
             
-                            //let memo_object = {
-                            //    from: memoFromKey,
-                            //    to: memoToKey,
-                            //    nonce,
-                            //    message: Aes.encrypt_with_checksum(
-                            //        pKey,
-                            //        memoToKey,
-                            //        nonce,
-                            //        memo
-                            //    )
-                            //}
+            //                 //let memo_object = {
+            //                 //    from: memoFromKey,
+            //                 //    to: memoToKey,
+            //                 //    nonce,
+            //                 //    message: Aes.encrypt_with_checksum(
+            //                 //        pKey,
+            //                 //        memoToKey,
+            //                 //        nonce,
+            //                 //        memo
+            //                 //    )
+            //                 //}
             
-                            let tr = new TransactionBuilder()
+            //                 let tr = new TransactionBuilder()
             
-                            tr.add_type_operation( "transfer", {
-                                fee: {
-                                    amount: 0,
-                                    asset_id: feeAsset.get("id")
-                                },
-                                from: fromAccount.get("id"),
-                                to: toAccount.get("id"),
-                                amount: { amount: sendAmount.amount, asset_id: sendAsset.get("id") }//,
-                                //memo: memo_object
-                            } )
+            //                 tr.add_type_operation( "transfer", {
+            //                     fee: {
+            //                         amount: 0,
+            //                         asset_id: feeAsset.get("id")
+            //                     },
+            //                     from: fromAccount.get("id"),
+            //                     to: toAccount.get("id"),
+            //                     amount: { amount: sendAmount.amount, asset_id: sendAsset.get("id") }//,
+            //                     //memo: memo_object
+            //                 } )
             
-                            tr.set_required_fees().then(() => {
-                                tr.add_signer(pKey, pKey.toPublicKey().toPublicKeyString());
-                                console.log("serialized transaction:", tr.serialize());
-                                console.log("Now I will broadcast")
-                                tr.broadcast();
-                            })
-                        });
-                //});
-            });
+            //                 tr.set_required_fees().then(() => {
+            //                     tr.add_signer(pKey, pKey.toPublicKey().toPublicKeyString());
+            //                     console.log("serialized transaction:", tr.serialize());
+            //                     console.log("Now I will broadcast")
+            //                     tr.broadcast();
+            //                 })
+            //             });
+            //     //});
+            // });
             
 
 
         }
         catch (ex) {
-            console.log(ex)
+            // console.log(ex)
             await this.setState({ isError: true })
             if (ex.message)
                 await this.setState({ error: ex.message })
